ce921bf2c7906a94a3dd30766fdd45170048c9d6
vidheeshnacode <vidheeshnacode@gmail.com>
2022-08-11 05:56:44 -0400

    Add Model

--- base/views.py
+++ base/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Room
+
+# rooms = [
+#     {'id':1, 'name':'Lets learn python!'},
+#     {'id':2, 'name':'Design with me'},
+#     {'id':3, 'name':'Backend Devs'},
+# ]
 
 def home(request):
-    return render(request, 'home.html')
+    rooms = Room.objects.all()
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
 
-def room(request):
-    return render(request, 'room.html')
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
